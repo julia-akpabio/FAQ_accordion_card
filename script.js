af38eb93bb12fadf4365fa57986a2a1dc517a0db
Julia Akpabio <jakpabio1@gmail.com>
2021-02-17 21:58:18 -0500

    refactored code, added javascript

---
+++ script.js
@@ -0,0 +1,15 @@
+let accordian = document.getElementsByClassName("question"); 
+let i; 
+
+for (i = 0; i < accordian.length; i++) {
+    accordian[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+
+        let panel = this.nextElementSibling; 
+        if (panel.style.display === "block") {
+            panel.style.display = "none"; 
+        } else {
+            panel.style.display = "block";
+        }
+    });
+}
